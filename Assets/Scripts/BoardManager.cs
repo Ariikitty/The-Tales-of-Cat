c44afa4c1cc3219647eaf62e2ecee75579da802b
Steveycat <Steveycat@users.noreply.github.com>
2019-12-13 12:01:49 +0000

    Added Unity Project

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
+public class BoardManager : MonoBehaviour
+{
+    // Using Serializable allows sub properties to be embeded in the unity editor
+    [Serializable]
+    public class Count
+    {
+        public int minimum;
+        public int maximun;
+
+        public Count (int min, int max)
+        {
+            minimum = min;
+            maximun = max;
+        }
+    }
+
+    //Setting the board size and prefabs the level generator will use for each object
+    public int columns = 8;
+    public int rows = 8;
+    public Count wallCount = new Count (5,9);
+    public Count foodCount = new Count (1,5);
+    public GameObject exit;
+    public GameObject[] floorTiles;
+    public GameObject[] wallTiles;
+    public GameObject[] foodTiles;
+    public GameObject[] enemyTiles;
+    public GameObject[] outerWallTiles;
+
+    private Transform boardHolder; //Stores a reference to the transform of the Board object
+    private List <Vector3> gridPositions = new List<Vector3>(); //A list of possible locations to place tiles
+
+    //Clears the list gridPositions and prepares it to generate a new board
+    void InitialiseList()
+    {
+        gridPositions.Clear();
+
+        for (int x = 1; x < columns - 1; x++)
+        {
+            for (int y = 1; y < rows - 1; y++)
+            {
+                gridPositions.Add(new Vector3(x,y,0f));
+            }
+        }
+    }
+
+    //Setting up the outer walls and floor of the game board
+    void BoardSetup ()
+    {
+        boardHolder = new GameObject ("Board").transform;
+
+        for (int x = -1; x < columns + 1; x++)
+        {
+            for (int y = -1; y < rows + 1; y++)
+            {
+                GameObject toInstantiate = floorTiles[Random.Range (0, floorTiles.Length)];
+                if (x == -1 || x == columns || y == -1 || y == rows)
+                    toInstantiate = outerWallTiles[Random.Range (0, outerWallTiles.Length)];
+
+                GameObject instance = Instantiate(toInstantiate, new Vector3 (x,y,0f), Quaternion.identity) as GameObject;
+
+                instance.transform.SetParent(boardHolder);
+            }
+        }
+    }
+
+    //RandomPosition returns a random position from the list of gridPositions
+    Vector3 RandomPosition()
+    {
+        int randomIndex = Random.Range(0, gridPositions.Count);
+        Vector3 randomPosition = gridPositions[randomIndex];
+        gridPositions.RemoveAt(randomIndex);
+        return randomPosition;
+    }
+
+    //LayoutObjectAtRandom accepts an array of game objects to choose from along with a minimum and maximum range for the number of objects to create
+    void LayoutObjectAtRandom(GameObject[] tileArray, int minimum, int maximun)
+    {
+        int objectCount = Random.Range (minimum, maximun + 1);
+
+        for (int i = 0; i < objectCount; i++)
+        {
+            Vector3 randomPosition = RandomPosition();
+            GameObject tileChoice = tileArray[Random.Range (0, tileArray.Length)];
+            Instantiate (tileChoice, randomPosition, Quaternion.identity);
+        }
+    }
+
+    //SetupScene initializes the level and calls the previous functions to lay out the game board
+    public void SetupScene (int level)
+    {
+        BoardSetup();
+        InitialiseList();
+        LayoutObjectAtRandom(wallTiles, wallCount.minimum, wallCount.maximun);
+        LayoutObjectAtRandom(foodTiles, foodCount.minimum, foodCount.maximun);
+        int enemyCount = (int)Mathf.Log(level, 2f);
+        LayoutObjectAtRandom(enemyTiles, enemyCount, enemyCount);
+        Instantiate(exit, new Vector3(columns - 1, rows -1, 0f), Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/MovingObject.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public abstract class MovingObject : MonoBehaviour
+{
+    public float moveTime = 1.0f;
+    public LayerMask blockingLayer;
+
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rb2D;
+    private float inverseMoveTime;
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        rb2D = GetComponent<Rigidbody2D>();
+        inverseMoveTime = 1f / moveTime;
+    }
+
+    //Move returns true if it is able to move and false if not
+    protected bool Move (int xDir, int yDir, out RaycastHit2D hit)
+    {
+        Vector2 start = transform.position;
+        Vector2 end = start + new Vector2 (xDir, yDir);
+        boxCollider.enabled = false;
+        hit = Physics2D.Linecast (start, end, blockingLayer);
+        boxCollider.enabled = true;
+
+        if (hit.transform == null)
+        {
+            StartCoroutine(SmoothMovement (end));
+            return true;
+        }
+
+        return false;
+    }
+
+    //Co-routine for moving units from one space to the next, takes a parameter end to specify where to move to
+    protected IEnumerator SmoothMovement (Vector3 end)
+    {
+        float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+
+        while (sqrRemainingDistance > float.Epsilon)
+        {
+            Vector3 newPosition = Vector3.MoveTowards (rb2D.position, end, inverseMoveTime * Time.deltaTime);
+            rb2D.MovePosition(newPosition);
+            sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+            yield return null;
+        }
+    }
+
+    //AttemptMove takes a generic perameter T to specify the type of component we expect our unit to interact with if blocked (Player for Enemies, Wall for Players)
+    protected virtual void AttemptMove <T> (int xDir, int yDir)
+        where T : Component
+    {
+        RaycastHit2D hit;
+        bool canMove = Move (xDir, yDir, out hit);
+
+        if (hit.transform == null)
+            return;
+
+        T hitComponent = hit.transform.GetComponent<T>();
+
+        if (!canMove && hitComponent != null)
+            OnCantMove(hitComponent);
+    }
+
+    //OnCantMove will be overriden by functions in the inheriting classes
+    protected abstract void OnCantMove <T> (T Component)
+        where T : Component;
+}
